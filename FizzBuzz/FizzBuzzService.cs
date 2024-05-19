651bd9f12598663a16dc71fe056f6ad1e5213232
Massimo Fazari <massimofazari@massimos-mbp.home>
2024-05-19 14:43:35 +0100

    Ammend Service

--- FizzBuzz/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzzService.cs
@@ -4,6 +4,8 @@
     {
         public string GetResult(int number)
         {
+            if (number % 15 == 0)
+                return "FizzBuzz";
             if (number % 3 == 0)
                 return "Fizz";
             return number.ToString();
