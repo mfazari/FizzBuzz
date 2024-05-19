394f915258254074b7e05d5b051ed3260625b80a
Massimo Fazari <massimofazari@massimos-mbp.home>
2024-05-19 14:39:00 +0100

    Add Fizz Test Case

---
+++ FizzBuzz/FizzBuzzService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FizzBuzz
+{
+	public class FizzBuzzService
+	{
+		public FizzBuzzService()
+		{
+		}
+	}
+}
+
---
+++ FizzBuzzTest/FizzBuzzServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using FizzBuzz;
+
+namespace FizzBuzzTest;
+
+public class FizzBuzzServiceTest
+{
+    [Fact]
+    public void TestFizzOutput()
+    {
+        // Arrange
+        var fizzBuzzService = new FizzBuzzService();
+
+        // Act
+        var result = fizzBuzzService.GetResult(3);
+
+        // Assert
+        Assert.Equal("Fizz", result);
+    }
+
+}
