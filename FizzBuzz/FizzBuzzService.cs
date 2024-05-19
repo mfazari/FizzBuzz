704a7418e42cd6398d058f7d5b6477c117d8f5a2
Massimo Fazari <massimofazari@massimos-mbp.home>
2024-05-19 14:47:23 +0100

    Add Buzz functionality

--- FizzBuzz/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzzService.cs
@@ -8,6 +8,8 @@
                 return "FizzBuzz";
             if (number % 3 == 0)
                 return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
             return number.ToString();
         }
     }
